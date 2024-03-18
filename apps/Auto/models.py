e95e6e04dec11bde155c6f64ec42930e7592ff45
telegbot <mersbmw3443@gmail.com>
2024-03-18 16:35:36 +0300

    git push

--- apps/Auto/models.py
+++ apps/Auto/models.py
@@ -4,8 +4,6 @@ from django.db import models
 
 class Auto(models.Model):
     title = models.CharField(max_length=255)
-    link = models.URLField()
-    user = models.ForeignKey()
 
     def __str__(self):
         return self.title
